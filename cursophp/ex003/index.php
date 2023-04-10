c7aad84d6f9ca7ac4b9813ec11707d39db77da6f
Andre <andrefelipe_santos@outlook.com>
2023-04-10 10:16:16 -0300

    ✨ feat(Variáveis e constantes):

---
+++ cursophp/ex003/index.php
@@ -0,0 +1,24 @@
+<?php 
+  $nome = "Gustavo";
+  $sobrenome = "Guanabara";
+  const PAIS = "Brasil";
+  //
+  //
+  $nome = "Gustavo";
+  $sobrenome = "Guanabara";
+  $idade = 27;
+  $peso = 102.3;
+  $casado = false;
+  echo "Muito prazer, $nome $sobrenome você mora no " . PAIS;
+
+  /*
+    Regras para nomes identificadores
+    1 - Variáveis sempre começam com símbolo $
+    2 - O segundo caracter de uma variável pode ser letra ou _
+    3 - Aceita caracteres [a-z], [A-Z], [0-9], [_]
+    4 - Aceita caracteres da tabela ASCII a partir de 128
+    5 - Aceita caracteres com ç, á...
+    6 - PHP é case sensitive em relação aos nomes
+    7 - NOmes especiais com $this não podem ser usadas 
+  */
+?>
